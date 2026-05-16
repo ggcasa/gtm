ccc4e356a99492948bdeeda033b0706e50c3ee80
ggcasa <casandru40@gmail>
2026-05-16 10:56:09 +0100

    feat: configurat server Go si structura PWA complet valida

--- cmd/gtm/main.go
+++ cmd/gtm/main.go
@@ -6,13 +6,12 @@ import (
 )
 
 func main() {
-	// 1. Servim fișierele statice generale (CSS, JS, Icons) din folderul web/static
-	// Ele vor fi accesibile la adresa: /static/js/app.js, etc.
 	fs := http.FileServer(http.Dir("./web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// 2. IMPORTANT PENTRU PWA: Service Worker-ul (sw.js) și Manifestul (manifest.json)
-	// Trebuie servite direct de la rădăcină (/) pentru ca PWA-ul să aibă scope complet.
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+	})
 	http.HandleFunc("/sw.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./web/sw.js")
 	})
